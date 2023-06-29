c20a04c8fb518994d06bf4fe5d017657c21eee12
manuelking <emmanuel_dunyo@outlook.com>
2023-06-29 12:36:10 +0100

    using img22 and lowering quality

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -6,13 +6,14 @@ const Hero = () => {
     <section className="flex sm:flex-row gap-y-16 flex-col-reverse py-6 ">
       <div className="relative flex-col flex flex-1 md:justify-end justify-center sm:items-start items-center">
         <Image
-          src="/img2.svg"
+          src="/img22.png"
           alt="Joe Gater Image 2"
           width={330}
           height={495}
           className="z-0 fill"
           priority={true}
-          quality={80}
+          quality={10}
+          style={{ objectFit: 'contain' }}
         />
       </div>
 
