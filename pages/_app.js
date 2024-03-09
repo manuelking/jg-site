8846132d65d3417424599f800652df447600d984
manuelking <emmanuel_dunyo@outlook.com>
2024-03-09 11:22:53 +0000

    moved vercel analytics to app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 const inter = Inter({
   subsets: ['latin'],
@@ -9,6 +10,7 @@ const inter = Inter({
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${inter.variable} font-sans`}>
+      <Analytics />
       <Component {...pageProps} />
     </main>
   )
--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
-import { Analytics } from '@vercel/analytics/react'
-
 export default function Document() {
   return (
     <Html lang="en">
@@ -11,7 +9,6 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <Analytics />
       </body>
     </Html>
   )
